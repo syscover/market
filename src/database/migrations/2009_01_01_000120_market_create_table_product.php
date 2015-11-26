4225e7fa534b148263a5f0d6e06a25f599146628
Jose Carlos Rodríguez Palacín <cpalacin@syscover.com>
2015-11-26 22:04:21 +0100

    add file group relation

--- src/database/migrations/2009_01_01_000120_market_create_table_product.php
+++ src/database/migrations/2009_01_01_000120_market_create_table_product.php
@@ -14,11 +14,14 @@ class MarketCreateTableProduct extends Migration
             Schema::create('012_111_product', function($table) {
                 $table->engine = 'InnoDB';
                 $table->increments('id_111')->unsigned();
-
+				$table->integer('custom_field_group_111')->unsigned()->nullable();
 				$table->boolean('active_111');
 
 				$table->string('data_lang_111',255)->nullable();
 				$table->text('data_111')->nullable();
+
+				$table->foreign('custom_field_group_111', 'fk01_012_111_product')->references('id_025')->on('001_025_field_group')
+					->onDelete('restrict')->onUpdate('cascade');
             });
 	}
 
