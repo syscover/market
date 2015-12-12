ac97366c5a16ad31e24f35ea7e6dd75be5e18549
Jose Carlos Rodríguez Palacín <cpalacin@syscover.com>
2015-12-12 13:15:37 +0100

    minor fixed

--- src/views/products/index.blade.php
+++ src/views/products/index.blade.php
@@ -26,7 +26,6 @@
 
 @section('tHead')
     <!-- market::products.index -->
-
     <tr>
         <th data-hide="phone,tablet">ID.</th>
         <th data-class="expand">{{ trans('pulsar::pulsar.name') }}</th>
