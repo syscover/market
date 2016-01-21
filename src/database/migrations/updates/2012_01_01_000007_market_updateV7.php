9bb205897ef3db8e5f3ba303354fd8ac46bb860e
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-01-21 19:25:07 +0100

    set default on invoiced

--- src/database/migrations/updates/2012_01_01_000007_market_updateV7.php
+++ src/database/migrations/updates/2012_01_01_000007_market_updateV7.php
@@ -16,7 +16,7 @@ class MarketUpdateV7 extends Migration
 			Schema::table('012_116_order', function ($table) {
 
 				$table->boolean('has_invoice_116')->after('invoice_longitude_116');
-				$table->boolean('invoiced_116')->after('has_invoice_116');
+				$table->boolean('invoiced_116')->default(false)->after('has_invoice_116');
 				$table->boolean('has_shipping_116')->after('invoiced_116');
 			});
 		}
