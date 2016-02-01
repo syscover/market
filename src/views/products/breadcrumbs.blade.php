14fbb3daff0dc6b835d2b67ba3abc87acd910e00
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-02-01 16:48:51 +0100

    minor fixed

--- src/views/products/breadcrumbs.blade.php
+++ src/views/products/breadcrumbs.blade.php
@@ -1,8 +1,8 @@
-<!-- market::profiles.breadcrumbs -->
+<!-- market::products.breadcrumbs -->
 <li>
     <a href="javascript:void(0)">{{ trans('market::pulsar.package_name') }}</a>
 </li>
 <li class="current">
     <a href="{{ route($routeSuffix, [session('baseLang')]) }}">{{ trans_choice($objectTrans, 2) }}</a>
 </li>
-<!-- /market::profiles.breadcrumbs -->
+<!-- /market::products.breadcrumbs -->
