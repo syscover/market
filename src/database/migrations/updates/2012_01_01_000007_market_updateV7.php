cf20ee55a247c186b406dd1a3695761aa20cbb51
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-02-15 20:25:39 +0100

    update v7

--- src/database/migrations/updates/2012_01_01_000007_market_updateV7.php
+++ src/database/migrations/updates/2012_01_01_000007_market_updateV7.php
@@ -11,10 +11,9 @@ class MarketUpdateV7 extends Migration
 	 */
 	public function up()
 	{
-		if(!Schema::hasColumn('012_116_order', 'provide_invoice_116'))
+		if(!Schema::hasColumn('012_116_order', 'has_invoice_116'))
 		{
 			Schema::table('012_116_order', function ($table) {
-
 				$table->boolean('has_invoice_116')->after('invoice_longitude_116');
 				$table->boolean('invoiced_116')->default(false)->after('has_invoice_116');
 				$table->boolean('has_shipping_116')->after('invoiced_116');
