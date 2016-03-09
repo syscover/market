d21044f3099bddedbbcfb8c6778c088c86c4c4b0
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-10 00:40:02 +0100

    implements active_126

--- src/Syscover/Market/Libraries/CouponLibrary.php
+++ src/Syscover/Market/Libraries/CouponLibrary.php
@@ -63,7 +63,7 @@ class CouponLibrary
                     ]
                 ];
             }
-            elseif(CustomerDiscountUsed::builder()->where('customer_126', auth('crm')->user()->id_301)->where('coupon_code_126', $couponCode)->count() >= $cartPriceRule->uses_customer_120)
+            elseif(CustomerDiscountUsed::builder()->where('customer_126', auth('crm')->user()->id_301)->where('coupon_code_126', $couponCode)->where('active_126', true)->count() >= $cartPriceRule->uses_customer_120)
             {
                 $errors[] = [
                     'status'    => 'error',
--- src/database/migrations/updates/2012_01_01_000008_market_updateV13.php
+++ src/database/migrations/updates/2012_01_01_000008_market_updateV13.php
@@ -14,7 +14,7 @@ class MarketUpdateV13 extends Migration
 		if( ! Schema::hasColumn('012_126_customer_discount_used', 'active_126'))
 		{
 			Schema::table('012_126_customer_discount_used', function ($table) {
-				$table->boolean('active_126')->default(true);
+				$table->boolean('active_126')->default(true)->after('order_126');
 				$table->index('active_126', 'ix04_012_126_customer_discount_used');
 			});
 		}
