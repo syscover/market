3b31c7e120416d7004fd7f20876b825d877d51b7
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-05-25 22:12:48 +0200

    updates

--- src/database/migrations/updates/2012_01_01_000016_market_updateV16.php
+++ src/database/migrations/updates/2012_01_01_000016_market_updateV16.php
@@ -18,6 +18,13 @@ class MarketUpdateV16 extends Migration
 				$table->decimal('tax_percentage_117', 10, 2)->after('discount_amount_117');
 			});
 		}
+
+		if(! Schema::hasColumn('012_117_order_row', 'tax_amount_117'))
+		{
+			Schema::table('012_117_order_row', function (Blueprint $table) {
+				$table->decimal('tax_amount_117', 10, 2)->after('tax_percentage_117');
+			});
+		}
 	}
 
 	/**
