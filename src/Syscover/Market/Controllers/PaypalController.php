389819089694e7804b3ccb71d241faccea56b2cd
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-08-26 22:38:38 +0200

    minor fix

--- src/Syscover/Market/Controllers/PaypalController.php
+++ src/Syscover/Market/Controllers/PaypalController.php
@@ -261,7 +261,8 @@ class PayPalController extends Controller
         $inputFields = new \PayPal\Api\InputFields();
         // Enables the buyer to enter a note to the merchant on the PayPal page during checkout.
         $inputFields->setAllowNote(true)
-            // Determines whether or not PayPal displays shipping address fields on the experience pages. Allowed values: 0, 1, or 2. When set to 0, PayPal displays the shipping address on the PayPal pages. When set to 1, PayPal does not display shipping address fields whatsoever. When set to 2, if you do not pass the shipping address, PayPal obtains it from the buyer’s account profile. For digital goods, this field is required, and you must set it to 1.
+            // Determines whether or not PayPal displays shipping address fields on the experience pages.
+            // Allowed values: 0, 1, or 2. When set to 0, PayPal displays the shipping address on the PayPal pages. When set to 1, PayPal does not display shipping address fields whatsoever. When set to 2, if you do not pass the shipping address, PayPal obtains it from the buyer’s account profile. For digital goods, this field is required, and you must set it to 1.
             ->setNoShipping(1)
             // Determines whether or not the PayPal pages should display the shipping address and not the shipping address on file with PayPal for this buyer. Displaying the PayPal street address on file does not allow the buyer to edit that address. Allowed values: 0 or 1. When set to 0, the PayPal pages should not display the shipping address. When set to 1, the PayPal pages should display the shipping address.
             ->setAddressOverride(0);
--- src/database/seeds/MarketResourceSeeder.php
+++ src/database/seeds/MarketResourceSeeder.php
@@ -9,18 +9,22 @@ class MarketResourceSeeder extends Seeder {
     {
         Resource::insert([
             ['id_007' => 'market',                      'name_007' => 'Market Package',                     'package_id_007' => '12'],
+            ['id_007' => 'market-cart-price-rule',      'name_007' => 'Marketing -- Cart price rule',       'package_id_007' => '12'],
+            ['id_007' => 'market-category',             'name_007' => 'Categories',                         'package_id_007' => '12'],
+            ['id_007' => 'market-order',                'name_007' => 'Orders',                             'package_id_007' => '12'],
+            ['id_007' => 'market-order-status',         'name_007' => 'Order status',                       'package_id_007' => '12'],
+            ['id_007' => 'market-payment-method',       'name_007' => 'Payment methods',                    'package_id_007' => '12'],
+            ['id_007' => 'market-product',              'name_007' => 'Products',                           'package_id_007' => '12'],
             ['id_007' => 'market-tax',                  'name_007' => 'Taxes',                              'package_id_007' => '12'],
             ['id_007' => 'market-tax-customer',         'name_007' => 'Taxes -- Customer class tax',        'package_id_007' => '12'],
             ['id_007' => 'market-tax-customer-group',   'name_007' => 'Taxes -- Group Customer class tax',  'package_id_007' => '12'],
             ['id_007' => 'market-tax-product',          'name_007' => 'Taxes -- Product class tax',         'package_id_007' => '12'],
             ['id_007' => 'market-tax-rate-zone',        'name_007' => 'Taxes -- Tax rate zone',             'package_id_007' => '12'],
             ['id_007' => 'market-tax-rule',             'name_007' => 'Taxes -- Tax rule',                  'package_id_007' => '12'],
-            ['id_007' => 'market-category',             'name_007' => 'Categories',                         'package_id_007' => '12'],
-            ['id_007' => 'market-product',              'name_007' => 'Products',                           'package_id_007' => '12'],
-            ['id_007' => 'market-order-status',         'name_007' => 'Order status',                       'package_id_007' => '12'],
-            ['id_007' => 'market-payment-method',       'name_007' => 'Payment methods',                    'package_id_007' => '12'],
-            ['id_007' => 'market-order',                'name_007' => 'Orders',                             'package_id_007' => '12'],
-            ['id_007' => 'market-cart-price-rule',      'name_007' => 'Marketing -- Cart price rule',       'package_id_007' => '12'],
+            ['id_007' => 'market-tpv',                  'name_007' => 'TPVs',                               'package_id_007' => '12'],
+            ['id_007' => 'market-tpv-paypal',           'name_007' => 'TPVs -- PayPal',                     'package_id_007' => '12'],
+            ['id_007' => 'market-tpv-paypal-setting',   'name_007' => 'TPVs -- PayPal -- Setting',          'package_id_007' => '12'],
+
         ]);
     }
 }
