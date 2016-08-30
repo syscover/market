0c3910b753b6af193de65fd60d4919b1089c2a65
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-08-30 02:34:17 +0200

    create web profile controller

--- src/Syscover/Market/Controllers/PayPalWebProfileController.php
+++ src/Syscover/Market/Controllers/PayPalWebProfileController.php
@@ -26,7 +26,18 @@ class PayPalController extends Controller
 {
     private $apiContext;
     private $preferences;
-    
+    private $webProfile;
+    protected $viewParameters = [
+        'newButton'             => true,    // button from index view to create record
+        'cancelButton'          => true,    // button from form view to cancel and return to index
+        'checkBoxColumn'        => true,    // checkbox from index view to select various records
+        'showButton'            => false,   // button from ajax response, to view record
+        'editButton'            => true,    // button from ajax response, to edit record
+        'deleteButton'          => true,    // button from ajax response, to delete record
+        'deleteSelectButton'    => true,    // button delete records when select checkbox on index view
+        'relatedButton'         => false,   // button to related elements, used on modal windows
+    ];
+
     /**
      * PayPalController constructor.
      * @param Request $request
@@ -240,5 +251,91 @@ class PayPalController extends Controller
 
             return view('pulsar::common.views.html_display', $viewResponse);
         }
+    }
+
+    public function webProfiles()
+    {
+        try
+        {
+            $response['webProfiles'] = \PayPal\Api\WebProfile::get_list($this->apiContext);
+        }
+        catch (\PayPal\Exception\PayPalConnectionException $e)
+        {
+            dd($e);
+        }
+
+        // set viewParamentes on parameters for throw to view
+        $response['viewParameters'] = $this->viewParameters;
+
+        return view('market::paypal_web_profile.index', $response);
+    }
+
+    public function createWebProfile()
+    {
+        // ### Create Web Profile
+        // Use the /web-profiles resource to create seamless payment experience profiles. See the payment experience overview for further information about using the /payment resource to create the PayPal payment and pass the experience_profile_id.
+        // Documentation available at https://developer.paypal.com/webapps/developer/docs/api/#create-a-web-experience-profile
+
+        // Lets create an instance of FlowConfig and add
+        // landing page type information
+        $flowConfig = new \PayPal\Api\FlowConfig();
+
+        // Type of PayPal page to be displayed when a user lands on the PayPal site for checkout.
+        // Allowed values: Billing or Login. When set to Billing, the Non-PayPal account landing page is used. When set to Login, the PayPal account login landing page is used.
+        $flowConfig->setLandingPageType("Login");
+
+        // The URL on the merchant site for transferring to after a bank transfer payment.
+        $flowConfig->setBankTxnPendingUrl('http://urbansafari.es');
+
+        // Parameters for style and presentation.
+        $presentation = new \PayPal\Api\Presentation();
+
+        // A URL to logo image. Allowed vaues: .gif, .jpg, or .png.
+        $presentation->setLogoImage('http://urbansafari.es/images/logo-urbansafari-paypal.png')
+            //	A label that overrides the business name in the PayPal account on the PayPal pages.
+            ->setBrandName("urbansafari.es")
+            //  Locale of pages displayed by PayPal payment experience.
+            ->setLocaleCode("ES");
+
+        // Parameters for input fields customization.
+        $inputFields = new \PayPal\Api\InputFields();
+        // Enables the buyer to enter a note to the merchant on the PayPal page during checkout.
+        $inputFields->setAllowNote(true)
+            // Determines whether or not PayPal displays shipping address fields on the experience pages.
+            // Allowed values: 0, 1, or 2. When set to 0, PayPal displays the shipping address on the PayPal pages. When set to 1, PayPal does not display shipping address fields whatsoever. When set to 2, if you do not pass the shipping address, PayPal obtains it from the buyer’s account profile. For digital goods, this field is required, and you must set it to 1.
+            ->setNoShipping(1)
+            // Determines whether or not the PayPal pages should display the shipping address and not the shipping address on file with PayPal for this buyer. Displaying the PayPal street address on file does not allow the buyer to edit that address. Allowed values: 0 or 1. When set to 0, the PayPal pages should not display the shipping address. When set to 1, the PayPal pages should display the shipping address.
+            ->setAddressOverride(0);
+
+        // #### Payment Web experience profile resource
+        $webProfile = new \PayPal\Api\WebProfile();
+
+        // Name of the web experience profile. Required. Must be unique
+        $webProfile->setName("UBANSAFARI" . uniqid())
+            // Parameters for flow configuration.
+            ->setFlowConfig($flowConfig)
+            // Parameters for style and presentation.
+            ->setPresentation($presentation)
+            // Parameters for input field customization.
+            ->setInputFields($inputFields);
+
+        // For Sample Purposes Only.
+        $this->request = clone $webProfile;
+
+        try
+        {
+            // Use this call to create a profile.
+            $createProfileResponse = $webProfile->create($this->apiContext);
+        }
+        catch (\PayPal\Exception\PayPalConnectionException $ex)
+        {
+            //\ResultPrinter::printError("Created Web Profile", "Web Profile", null,  $this->request, $ex);
+            exit(1);
+        }
+        var_dump($createProfileResponse);
+
+        //ResultPrinter::printResult("Created Web Profile", "Web Profile", $createProfileResponse->getId(),  $this->request, $createProfileResponse);
+
+        //return $createProfileResponse;
     }
 }
