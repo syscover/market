c17b34814c5f55e340b32d987bd3edda20a6f65d
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-08-30 02:16:46 +0200

    index web profiles

---
+++ src/views/paypal_web_profile/index.blade.php
@@ -0,0 +1,44 @@
+@extends('pulsar::layouts.index')
+
+@section('head')
+    @parent
+    <!-- market::order_status.index -->
+    <script>
+        $(document).ready(function() {
+            if ($.fn.dataTable)
+            {
+                $('.datatable-pulsar').dataTable({
+                    "displayStart": {{ $offset }},
+                    "columnDefs": [
+                        { "sortable": false, "targets": [4,5]},
+                        { "class": "checkbox-column", "targets": [4]},
+                        { "class": "align-center", "targets": [3,5]}
+                    ],
+                    "processing": true,
+                    "serverSide": true,
+                    "ajax": {
+                        "url": "{{ route('jsonData' . ucfirst($routeSuffix), [base_lang()->id_001]) }}",
+                        "type": "POST",
+                        "headers": {
+                            "X-CSRF-TOKEN": "{{ csrf_token() }}"
+                        }
+                    }
+                }).fnSetFilteringDelay();
+            }
+        });
+    </script>
+    <!-- market::order_status.index -->
+@stop
+
+@section('tHead')
+    <!-- market::order_status.index -->
+    <tr>
+        <th data-hide="phone,tablet">ID.</th>
+        <th data-hide="phone,tablet">{{ trans_choice('pulsar::pulsar.language', 1) }}</th>
+        <th data-class="expand">{{ trans('pulsar::pulsar.name') }}</th>
+        <th data-hide="phone">{{ trans_choice('pulsar::pulsar.active', 1) }}</th>
+        <th class="checkbox-column"><input type="checkbox" class="uniform"></th>
+        <th>{{ trans_choice('pulsar::pulsar.action', 2) }}</th>
+    </tr>
+    <!-- /market::order_status.index -->
+@stop
